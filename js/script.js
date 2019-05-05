c3a5ba2916989ac425161b7786413729bf4b3210
Filipp <fis_@mail.ru>
2019-05-05 19:30:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,91 +1,52 @@
-window.addEventListener('DOMContentLoaded', function() {              //load говорит js, что сейчас страница будет загружаться и только после того 
-                                                                    //как загрузиться полностью всё до последней картинки, js скрипт будет выполняться 
-'use strict';                                                        //DomContentLoaded это событие срабатывает, когда полностью загрузилась DOM структура документа
-let tab = document.querySelectorAll('.info-header-tab'),
-info = document.querySelector('.info-header'),
-tabContent = document.querySelectorAll('.info-tabcontent'),
-photosFromTrips = document.getElementsByClassName('slider-item fade'),
-prevFoto = document.getElementsByClassName('arrow-left'),
-nextFoto = document.getElementsByClassName('arrow-right');
-//console.log(photosFromTrips[1]);
-console.log(prevFoto);
+let deadline = '2022-06-07';
 
+function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date())
 
+    if (t < 0){
+        t = 0
+    }
+    seconds = Math.floor((t/1000) % 60),
+    minutes = Math.floor((t/1000/60) % 60),
+    hours1 = Math.floor((t/(1000*60*60))),
+    hours = Math.floor(t/(1000*60*60) % 24);
+    // days = Math.floor(t/(1000*60*60*24));
 
-function hideTabContent(a) {
-for(let i = a; i < tabContent.length; i++){
-tabContent[i].classList.remove('show');                 //удаляем класс show
-tabContent[i].classList.add('hide');                    //скроем все елементы tabContent со страницы
-}
-}
+            if (hours < 10) hours = "0" + hours;
+            if (minutes < 10) minutes  = "0" + minutes;
+            if (seconds < 10) seconds  = "0" + seconds;
+            // if (days < 10) days = "0" + days;
 
-hideTabContent("1");                                     //скрыват все tabContent кроме первого
-
-function showTabContent(b) {
-if (tabContent[b].classList.contains('hide')){
-tabContent[b].classList.remove('hide');                 //удаляем(скрываем) класс show
-tabContent[b].classList.add('show');
-}
-}
-
-info.addEventListener('click', function(event){
-let target = event.target;
-if(target && target.classList.contains('info-header-tab'))  {
-for(let i = 0; i < tab.length; i++) {                   //цикл будет работать до момента когда закончатся tab-ы
-if(target == tab[i]){
-hideTabContent(0);
-showTabContent(i);
-break;
-}
-} 
-}
-});
-//timer
-
-let deadline = '2019-04-09';                                            //задали конечную дату
-
-function getTimeRemaining(endtime) {                                    //функция считает между датой(сейчас) и deadline
-let t = Date.parse(endtime) - Date.parse(new Date())
-if (t < 0){                                                             //таймер в минус не уходит
-t = 0 
-}                                                                       //сюда помещаем разницу между датами. parse превращает
-let seconds = Math.floor((t/1000) % 60),                                //любую дату в колличество милисекунд                                                        
-minutes = Math.floor((t/1000/60) % 60),                                 //делим на 1000 чтобы получить к-во целых секунд, затем на 60-целых минут
-hours = Math.floor((t/(1000*60*60)));                                   //количество целых часов
-// hours = math.floor((t/1000/60/60) % 24),
-// days = math.floor((t/(1000*60*60*24)))
-
-
-return {
-'total' : t,
-'hours' : hours,
-'minutes' : minutes,
-'seconds' : seconds
-};
-
+    return {
+        'total' : t,
+        'hours' : hours,
+        'minutes' : minutes,
+        'seconds' : seconds        
+    };
 }
 
 function setClock(id, endtime) {
-let timer = document.getElementById(id),
+    let timer = document.getElementById(id),
+        // days = document.querySelector('.days'),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock, 1000);
 
-hours = timer.querySelector('.hours'),
-minutes = timer.querySelector('.minutes'),
-seconds = timer.querySelector('.seconds'),
-timeInterval = setInterval(updateClock, 1000);
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            // days.textContent = t.days;
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
 
-
-function updateClock() {
-let t = getTimeRemaining(endtime);
-hours.textContent = ('0' + t.hours).slice(-2);
-minutes.textContent = ('0' + t.minutes).slice(-2);
-seconds.textContent = ('0'+ t.seconds).slice(-2);
-
-if (t.total <= 0) {
-clearInterval(timeInterval);
-}
-}
-
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+            
+        }
 }
 
 setClock('timer', deadline);
-}); 
+
+
